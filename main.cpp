51a32af34a8f02d7fdb64ac06ba0df219a6d0b5d
Kaitlyn O’Flaherty <kaitlynoflaherty@Kaitlyns-MacBook-Pro.local>
2022-10-11 17:29:40 -0400

    fixing main

--- main.cpp
+++ main.cpp
@@ -7,36 +7,6 @@
 
 using namespace std;
 
-int calculate_points(int total_points, int value, string suit)
-// Function to calculate the points per card chosen
-{
-    if (value == 1)
-    {
-        total_points += 10;
-    }
-    else if ((value >= 11) && (value <= 13))
-    {
-        total_points += 5;
-    }
-    else if (value == 7)
-    {
-        total_points = total_points / 2;
-        round(total_points);
-    }
-    else if ((value >= 2 && value <= 6))
-    {
-        total_points = 0;
-    }
-    else{}
-    
-    if (suit == "Heart")
-    {
-        total_points += 1;
-    }
-    
-    return total_points;
-} // End calculate_points
-
 int main()
 {
     //create deck & print
@@ -52,18 +22,4 @@ int main()
     d_52.shuffle();
     cout << d_52;
 
-    // Initialize points
-    int total_points = 0;
-
-    //have user choose which card they want to flip
-
-
-    //calculate points
-    calculate_points(total_points, value, suit);
-
-    // print total points 
-    cout << "You currently have " << total_points << " points." << endl;
-
-    //ask user if they want to continue or end game
-    
 } // End main
