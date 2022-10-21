4251a4b71995f6c846e98aa6db7243140611505b
Lisa Byrne <byrne.li@northeastern.edu>
2022-10-21 15:53:19 -0400

    started writing files for global function playflip

---
+++ play_flip.h
@@ -0,0 +1,9 @@
+// Project #2: Flip Card part b
+// 
+// Group Members: Lisa Byrne, Alek Tunik, Kaite O'Flaherty
+//
+// Description: Header file for global function to play flip card
+
+#pragma once
+
+void playFlip();
