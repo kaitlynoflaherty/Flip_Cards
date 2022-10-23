a0479f6161748d5af83a3007dbef2c221652ce36
Kaitlyn O’Flaherty <kaitlynoflaherty@Kaitlyns-MacBook-Pro.local>
2022-10-22 22:28:32 -0500

    updates to main

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int play_flip()
 // Function to 
 {
     //initialize variables
-    int total_points = 0, position = 0, choice;
+    int total_points = 0, position = 0, prev_position = 0, choice;
 
     //initiate game
     // create deck & print
@@ -36,69 +36,93 @@ int play_flip()
     d_52.shuffle();
     cout << "Shuffled deck:" << endl << d_52 << endl;
 
-    //draw 24 cards to 'd_current' & print
+    // Move 24 cards to 'd_current' (new deck) & print
     deck d_current;
     card temp_card;
+    node<card> *front;
+    front = NULL;
+
     for (int i = 0; i < 24; i++)
     {
         temp_card = d_52.deal();
-        d_current = temp_card.replace(node<card> *bottom_card);
+        if (front == NULL)
+        {
+            front = new node<card>(temp_card);
+        }
+        else
+        {
+            d_current.replace(temp_card);
+        }
+
     }
+
     //print 52 card deck & 24 card deck
-    cout << "d_52: " << endl; << d_52 << endl;
+    cout << "d_52: " << endl << d_52 << endl;
     cout << "d_current:" << endl << d_current << endl;
 
     //do while loop which continues if user does not enter 'NO'
     do
     {
-    //print current points
-    cout << "You have " << total_points << "points." << endl;
+        //print current points
+        cout << "You have " << total_points << "points." << endl;
 
-    //ask user if they want to continue playing
-    cout << "Would you like to continue playing? Choose 1 to continue and" 
-    << "0 to quit." << endl;
-    cin >> choice;
+        //ask user if they want to continue playing
+        cout << "Would you like to continue playing? Choose 1 to continue and" 
+        << "0 to quit." << endl;
+        cin >> choice;
 
-    //ask user to choose a position 1-24 
-    cout << "Please choose a card from position 1 - 24" << endl;
-    cin >> position;
+        //ask user to choose a position 1-24 
+        cout << "Please choose a card from position 1 - 24" << endl;
+        cin >> position;
     
-    // range check for position 1 - 24
-    if (position < 1 || position > 24 )
-    {
-        cout << "This card is out of range." << endl;
-    }
+        // range check for position 1 - 24
+        if (position < 1 || position > 24 )
+        {
+            cout << "This card is out of range." << endl;
+        }
 
-    // select a card, return value & suit
+        // BONUS: avoid flipping same card
+        // idea: save each value already guessed to a container and compare each 
+        // new guess to the previous guesses w/ loop & comparison
 
+
+        // select a card, return value & suit
+        // BONUS: don't remove card
+
+
+        value = card_choice.getValue()
+        suit = card-choice.getSuit()
     
-    //calculate points
-    if (value == 1)
-    {
-        total_points += 10;
-    }
-    else if ((value >= 11) && (value <= 13))
-    {
-        total_points += 5;
-    }
-    else if (value == 7)
-    {
-        total_points = total_points / 2;
-        round(total_points);
-    }
-    else if ((value >= 2 && value <= 6))
-    {
-        total_points = 0;
-    }
-    else{}
+        //calculate points
+        if (value == 1)
+        {
+            total_points += 10;
+        }
+        else if ((value >= 11) && (value <= 13))
+        {
+            total_points += 5;
+        }
+        else if (value == 7)
+        {
+            total_points = total_points / 2;
+            round(total_points);
+        }
+        else if ((value >= 2 && value <= 6))
+        {
+            total_points = 0;
+        }
+        else{}
     
-    if (suit == "Heart")
-    {
-        total_points += 1;
-    }
+        if (suit == "Heart")
+        {
+            total_points += 1;
+        }
+
+        //update positon choice to previous choice to avoid repeated guesses
+        prev_position = position;
     
     } 
-    while (choice != 0); // end while loop
+    while (choice != 0); // end do while loop
 
     //print final points
     cout << "You finished with " << total_points << "points!" << endl;
@@ -118,6 +142,10 @@ int main()
     for (int i = 0; i < num_players; i++)
     {
         play_flip();
+        cout << "The next player can begin!" << endl;
     }
+
+    //compare player points and declare a winner (optional)
+
 
 } // End main
